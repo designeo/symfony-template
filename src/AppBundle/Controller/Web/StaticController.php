1f1780ee35be6a6b9302a1056d326ff79765c5ee
Petr Fidler <fidler@freshsevices.cz>
2015-10-22 16:58:08 +0200

    Static texts removed

--- src/AppBundle/Controller/Web/StaticController.php
+++ src/AppBundle/Controller/Web/StaticController.php
@@ -15,27 +15,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
  */
 class StaticController extends AbstractWebController
 {
-    /**
-     * @Route("/about-us", name="app_static_aboutus")
-     * @Template("AppBundle:Web/Static:about_us.html.twig")
-     *
-     * @return array
-     */
-    public function aboutUsAction()
-    {
-        return [];
-    }
-
-    /**
-     * @Route("/how-it-works", name="app_static_howitworks")
-     * @Template("AppBundle:Web/Static:how_it_works.html.twig")
-     *
-     * @return array
-     */
-    public function howItWorksAction()
-    {
-        return [];
-    }
 
     /**
      * @Route("/terms-of-use", name="app_static_termsofuse")
