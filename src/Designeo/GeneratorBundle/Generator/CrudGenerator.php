251c3f79db3435fccb7baae9ff3423b6bb4f09d0
Tomáš Polívka <tomas.polivka@designeo.cz>
2015-11-02 17:56:41 +0100

    Doc remove

--- src/Designeo/GeneratorBundle/Generator/CrudGenerator.php
+++ src/Designeo/GeneratorBundle/Generator/CrudGenerator.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Designeo\GeneratorBundle\CrudGenerator;
 
 use Sensio\Bundle\GeneratorBundle\Generator\Generator;
